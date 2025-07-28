1860aa1f5475db35ae1e84f1f02f83d54bb6da3f
Petro Danylevskyi <petro@danylevskyi.com>
2025-07-28 19:20:03 +0300

    Fix Encoder

--- EncoderCounter/EncoderCounter.ino
+++ EncoderCounter/EncoderCounter.ino
@@ -3,17 +3,26 @@ const int pinDt = 12;
 
 int clkLastState;
 int clkCurrentState;
+int dtCurrentState;
 
 void setup() {
     Serial.begin(9600);
     pinMode(pinClk, INPUT);
     pinMode(pinDt, INPUT);
+    clkLastState = digitalRead(pinClk);
 }
 
 void loop() {
+    dtCurrentState = digitalRead(pinDt);
     clkCurrentState = digitalRead(pinClk);
     if (clkCurrentState != clkLastState) {
-         Serial.println("Encoder changed.");
+        Serial.println("Encoder changed.");
+        if (dtCurrentState == clkCurrentState) {
+            Serial.println("Turned left.");
+        } else {
+            Serial.println("Turned right.");
+        }
     }
-    clkLastState = clkCurrentState;
+    clkLastState = clkCurrentState; 
+    delay(5);
 }
